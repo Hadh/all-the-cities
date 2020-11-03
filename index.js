6f73618c96b3a9d94093d58bc750ca395e5db19e
Hadhemi Laouini <hadhemi.laouini@esprit.tn>
2020-11-03 12:47:54 +0100

    added CORS

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const allCities = require('./cities');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use('/all-cities', allCities);
 
 const port = 5555;
